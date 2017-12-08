18c64c395d1ad0338702242ac8701f3614add8d3
量飞 <flyweights@163.com>
2017-12-08 23:28:13 +0800

    add HTML template and request

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def post_list(request):
+    """docstring for post_list"""
+    return render(request, 'blog/post_list.html', {})
