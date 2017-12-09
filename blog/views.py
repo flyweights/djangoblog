a55fb42b54933569e77d1cc35e6f950765414952
量飞 <flyweights@163.com>
2017-12-10 00:04:36 +0800

    add post_detail and css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.utils import timezone
@@ -10,3 +10,9 @@ def post_list(request):
     """docstring for post_list"""
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    """docstring for post_detail"""
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
